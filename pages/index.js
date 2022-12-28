a57acac804ba30b0f398310061c1f7af11ac94e5
sillybierba <sillybierba@gmail.com>
2022-12-27 21:46:56 -0800

    changed homepage
    changed homepage to match the task for gpt3

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,43 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react'
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Soviet Joke Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Soviet Jokes!</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>generate a Soviet joke about anything</h2>
           </div>
+        </div>
+        <div className='prompt-container'>
+          <textarea 
+            placeholder="start typing here"
+            class="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
         </div>
       </div>
       <div className="badge-container grow">
